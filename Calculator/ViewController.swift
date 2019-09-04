7840fa750b65806a04cc0a10374720a464fa7b85
Tim Li <massonlee@gmail.com>
2019-09-04 23:11:21 +1000

    Init demo

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Tim Li on 4/9/19.
+//  Copyright © 2019 Tim Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
